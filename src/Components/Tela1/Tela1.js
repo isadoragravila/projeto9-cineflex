a426f5c978e691b7485c1da31766bedaaa87ca58
isadoragravila <isadoragravila@gmail.com>
2022-05-22 22:51:14 -0300

    Styled components Tela1

--- src/Components/Tela1/Tela1.js
+++ src/Components/Tela1/Tela1.js
@@ -1,14 +1,14 @@
-import "./style.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 function Movie({ img, title, id }) {
   return (
     <Link to={`/sessoes/${id}`}>
-      <div className="filme">
+      <Filme>
         <img src={img} alt={title} />
-      </div>
+      </Filme>
     </Link>
   );
 }
@@ -17,27 +17,51 @@ export default function Tela1() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const promise = axios.get(
-      "https://mock-api.driven.com.br/api/v5/cineflex/movies"
-    );
-    promise.then((response) => {
-      setMovies(response.data);
-    });
+    const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+    promise.then((response) => setMovies(response.data));
   }, []);
 
   return (
-    <div className="tela1">
+    <Conteiner>
       <h2>Selecione o filme</h2>
-      <div className="filmes">
-        {movies.map((movie, index) => (
-          <Movie
-            key={index}
-            img={movie.posterURL}
-            title={movie.title}
-            id={movie.id}
-          />
-        ))}
-      </div>
-    </div>
+      <Filmes>
+        {movies.map((movie, index) => (<Movie key={index} img={movie.posterURL} title={movie.title} id={movie.id} />))}
+      </Filmes>
+    </Conteiner>
   );
 }
+
+const Conteiner = styled.div`
+  width: 375px;
+  margin-top: 108px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 25px;
+`;
+
+const Filmes = styled.div`
+  margin-top: 41px;
+  width: 325px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const Filme = styled.div`
+  width: 145px;
+  height: 209px;
+  background: #ffffff;
+  box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+  margin-bottom: 11px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 129px;
+    height: 193px;
+    object-fit: cover;
+  }
+`;
