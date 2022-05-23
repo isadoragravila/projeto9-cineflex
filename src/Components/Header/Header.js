94ea130d70b236ff4d3276977a264526116dca49
isadoragravila <isadoragravila@gmail.com>
2022-05-23 14:30:05 -0300

    Botão voltar v4

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,15 @@ import { useNavigate } from "react-router-dom";
 export default function Header({ home, setHome }) {
   let navigate = useNavigate();
   function handleClick() {
+    if (home === "/sessoes") {
+        setHome("/");
+      }
+      if (home === "/assentos") {
+        setHome("/sessoes");
+      }
+      if (home === "/sucesso") {
+        setHome("/assentos");
+      }
     navigate(-1);
   }
   return (
