03c56b1e5ab7fdf6382906be1aa4d7f050b9c839
isadoragravila <isadoragravila@gmail.com>
2022-05-23 00:27:05 -0300

    Styled components Tela3

--- src/Components/Tela3/Tela3.js
+++ src/Components/Tela3/Tela3.js
@@ -1,8 +1,37 @@
-import "./style.css";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Footer from "../Footer/Footer";
+import styled from "styled-components";
+
+function Cadeiras({ seat, selected, setSelected, ids, setIds }) {
+  const [click, setClick] = useState(true);
+
+  function clicked() {
+    setClick(!click);
+    if (click) {
+      setSelected([...selected, seat.name]);
+      setIds([...ids, seat.id]);
+    } else {
+      setSelected(selected.filter((item) => item !== seat.name));
+      setIds(ids.filter((item) => item !== seat.id));
+    }
+  }
+
+  return (
+    <>
+      {seat.isAvailable ? (
+        <Assento cor={click ? "#c3cfd9" : "#8dd7cf"} borda={click ? "#808f9d" : "#45bdb0"} onClick={clicked}>
+          {seat.name}
+        </Assento>
+      ) : (
+        <Assento cor={"#fbe192"} borda={"#f7c52b"} onClick={() => alert("Esse assento não está disponível")}>
+          {seat.name}
+        </Assento>
+      )}
+    </>
+  );
+}
 
 export default function Tela3({ setPedido, ids, setIds }) {
   const { idSessao } = useParams();
@@ -49,92 +78,130 @@ export default function Tela3({ setPedido, ids, setIds }) {
 
   return (
     <>
-      <div className="tela3">
+      <Conteiner>
         <h2>Selecione o(s) assento(s)</h2>
-        <div className="cadeiras">
-          {seats.map((seat, index) => (
-            <Cadeiras
-              key={index}
-              seat={seat}
-              selected={selected}
-              setSelected={setSelected}
-              ids={ids}
-              setIds={setIds}
-            />
-          ))}
-        </div>
-        <div className="indice">
-          <div className="legenda">
-            <div className="circulo verde"></div>
+        <Assentos>
+          {seats.map((seat, index) => (<Cadeiras key={index} seat={seat} selected={selected} setSelected={setSelected} ids={ids} setIds={setIds} />))}
+        </Assentos>
+        <Indice>
+          <Legenda>
+            <Circulo cor={"#8dd7cf"} borda={"#45bdb0"} />
             <p>Selecionado</p>
-          </div>
-          <div className="legenda">
-            <div className="circulo "></div>
+          </Legenda>
+          <Legenda>
+            <Circulo cor={"#c3cfd9"} borda={"#808f9d"} />
             <p>Disponível</p>
-          </div>
-          <div className="legenda">
-            <div className="circulo amarela"></div>
+          </Legenda>
+          <Legenda>
+            <Circulo cor={"#fbe192"} borda={"#f7c52b"} />
             <p>Indisponível</p>
-          </div>
-        </div>
-        <div className="dados">
+          </Legenda>
+        </Indice>
+        <Dados>
           <p>Nome do comprador:</p>
-          <input
-            type="text"
-            placeholder="Digite seu nome..."
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <input type="text" placeholder="Digite seu nome..." value={name} onChange={(e) => setName(e.target.value)} />
           <p>CPF do comprador:</p>
-          <input
-            type="text"
-            placeholder="Digite seu CPF..."
-            value={cpf}
-            onChange={(e) => setCpf(e.target.value)}
-          />
-        </div>
+          <input type="text" placeholder="Digite seu CPF..." value={cpf} onChange={(e) => setCpf(e.target.value)} />
+        </Dados>
         <div className="botao" onClick={reservar}>
           Reservar assento(s)
         </div>
-      </div>
-      <Footer
-        title={movie.title}
-        img={movie.posterURL}
-        time={time}
-        day={day.weekday}
-      />
+      </Conteiner>
+      <Footer title={movie.title} img={movie.posterURL} time={time} day={day.weekday} />
     </>
   );
 }
 
-function Cadeiras({ seat, selected, setSelected, ids, setIds }) {
-  const [click, setClick] = useState(true);
+const Conteiner = styled.div`
+  width: 375px;
+  margin-top: 108px;
+  margin-bottom: 117px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 25px;
+`;
 
-  function clicked() {
-    setClick(!click);
-    if (click) {
-      setSelected([...selected, seat.name]);
-      setIds([...ids, seat.id]);
-    } else {
-      setSelected(selected.filter((item) => item !== seat.name));
-      setIds(ids.filter((item) => item !== seat.id));
-    }
+const Assentos = styled.div`
+  margin-top: 41px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Assento = styled.div`
+background-color: ${props => props.cor};
+border: 1px solid ${(props) => props.borda};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #000000;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  font-size: 11px;
+  width: 26px;
+  height: 26px;
+  border-radius: 12px;
+  margin: 0 3.25px 18px 3.25px;
+  cursor: pointer;
+`;
+
+const Indice = styled.div`
+  display: flex;
+  width: 286px;
+  justify-content: space-between;
+  margin-bottom: 40px;
+`;
+
+const Legenda = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 13px;
+    color: #4e5a65;
+  }
+`;
+
+const Circulo = styled.div`
+  width: 25px;
+  height: 25px;
+  border-radius: 17px;
+  background-color: ${props => props.cor};
+  border: 1px solid ${(props) => props.borda};
+  margin-bottom: 8px;
+`;
+
+const Dados = styled.div`
+  width: 325px;
+  margin-bottom: 47px;
+
+  p {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    color: #293845;
   }
 
-  return (
-    <>
-      {seat.isAvailable ? (
-        <div className={click ? "cadeira" : "cadeira verde"} onClick={clicked}>
-          {seat.name}
-        </div>
-      ) : (
-        <div
-          className="cadeira amarela"
-          onClick={() => alert("Esse assento não está disponível")}
-        >
-          {seat.name}
-        </div>
-      )}
-    </>
-  );
-}
+  input {
+    width: 325px;
+    height: 51px;
+    margin-top: 5px;
+    margin-bottom: 10px;
+    background: #ffffff;
+    border: 1px solid #d5d5d5;
+    border-radius: 3px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    text-indent: 10px;
+    outline: none;
+  }
+
+  input::placeholder {
+    font-style: italic;
+    color: #afafaf;
+  }
+`;
